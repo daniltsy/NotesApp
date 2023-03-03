1183cacbdd5b8462f2fd3a98b5f6329e94d1f71f
daniltsy <cli0300@mail.ru>
2023-03-03 12:03:33 +0300

    Добавил иконки для различного отображения элементов компонента Notes

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,54 @@
     <div class="wrapper-content">
       <section>
         <div class="container">
-          <h1>{{ title }}</h1>
           <message v-if="message" :message="message"></message>
           <new-note :note="note" @addNote="addNote"></new-note>
-          <notes :notes="notes"></notes>
+          <div class="note-header">
+            <h1>{{ title }}</h1>
+            <div class="icons">
+              <svg
+                style="cursor: pointer"
+                :class="{ active: grid }"
+                @click="grid = true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <rect x="3" y="3" width="7" height="7"></rect>
+                <rect x="14" y="3" width="7" height="7"></rect>
+                <rect x="14" y="14" width="7" height="7"></rect>
+                <rect x="3" y="14" width="7" height="7"></rect>
+              </svg>
+              <svg
+                style="cursor: pointer"
+                :class="{ active: !grid }"
+                @click="grid = false"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <line x1="8" y1="6" x2="21" y2="6"></line>
+                <line x1="8" y1="12" x2="21" y2="12"></line>
+                <line x1="8" y1="18" x2="21" y2="18"></line>
+                <line x1="3" y1="6" x2="3" y2="6"></line>
+                <line x1="3" y1="12" x2="3" y2="12"></line>
+                <line x1="3" y1="18" x2="3" y2="18"></line>
+              </svg>
+            </div>
+          </div>
+          <notes :notes="notes" :grid="grid"></notes>
         </div>
       </section>
     </div>
@@ -28,6 +72,7 @@ export default {
     return {
       title: "Notes App",
       message: null,
+      grid: true,
       note: {
         title: "",
         descr: "",
--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="notes">
-    <div class="note" v-for="(note, index) in notes" :key="index">
-      <div class="note-header">
+    <div
+      class="note"
+      :class="{ full: !grid }"
+      v-for="(note, index) in notes"
+      :key="index"
+    >
+      <div class="note-header" :class="{ full: !grid }">
         <p>{{ note.title }}</p>
+        <p style="cursor: pointer" @click="removeNote(index)">x</p>
       </div>
       <div class="note-body">
         <p>{{ note.descr }}</p>
@@ -19,6 +25,15 @@ export default {
     notes: {
       type: Array,
       required: true,
+    },
+    grid: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    removeNote(index) {
+      this.notes.splice(index, 1)
     },
   },
 }
@@ -37,10 +52,46 @@ export default {
   padding: 10px 20px;
   margin-bottom: 20px;
   background-color: white;
+  transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
+  box-shadow: 0 30px 30px rgba(0, 0, 0, 0.02);
+  &:hover {
+    box-shadow: 0 30px 30px rgba(0, 0, 0, 0.04);
+    transform: translate(0, -10px);
+    transition-delay: 0s !important;
+  }
+  &.full {
+    width: 100%;
+    text-align: center;
+  }
 }
 .note-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  h1 {
+    font-size: 32px;
+  }
   p {
     color: mediumblue;
+  }
+  svg {
+    margin-right: 12px;
+    color: #999999;
+    &.active {
+      color: blue;
+    }
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+  &.full {
+    justify-content: center;
+    p {
+      margin-right: 16px;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
   }
 }
 .note-body {
