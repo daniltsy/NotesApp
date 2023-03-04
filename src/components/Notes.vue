860aa110bbcf3eb678af0cecdb7f29a4f2552240
daniltsy <cli0300@mail.ru>
2023-03-05 00:16:28 +0300

    Добавил возможность изменять тайтл показанных снизу записей при двойном нажатии и сохранении введенной информации с помощью Enter, а также отменять изменения при нажатии Esc или отведения фокуса

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -6,8 +6,19 @@
       v-for="(note, index) in notes"
       :key="index"
     >
-      <div class="note-header" :class="[{ full: !grid }, getClass(note)]">
-        <p>{{ note.title }}</p>
+      <div
+        class="note-header"
+        :class="[{ full: !grid }, getClass(note)]"
+        @dblclick="showHiddenInput(index)"
+      >
+        <p v-if="activeIndex !== index">{{ note.title }}</p>
+        <input
+          v-if="activeIndex === index"
+          type="text"
+          v-model="note.title"
+          @keydown="updateNoteTitle(index, $event)"
+          @blur="canselEditing(index)"
+        />
         <p style="cursor: pointer" @click="removeNote(index)">x</p>
       </div>
       <div class="note-body">
@@ -21,6 +32,14 @@
 <script>
 export default {
   name: "Notes",
+  data() {
+    return {
+      hiddenInput: false,
+      activeIndex: null,
+      initialTitle: "",
+      noBlur: false,
+    }
+  },
   props: {
     notes: {
       type: Array,
@@ -39,6 +58,29 @@ export default {
       if (note.priority === "Very Important") return "red"
       else if (note.priority === "Important") return "orange"
       else return "green"
+    },
+    showHiddenInput(index) {
+      this.activeIndex = index
+      this.hiddenInput = !this.hiddenInput
+      this.initialTitle = this.notes[index].title
+      this.noBlur = false
+    },
+    updateNoteTitle(index, event) {
+      if (event.keyCode === 13) {
+        this.notes[index].title = this.notes[index].title.trim()
+        this.activeIndex = null
+        this.hiddenInput = false
+        this.noBlur = true
+      } else if (event.keyCode === 27) {
+        this.canselEditing(index)
+      }
+    },
+    canselEditing(index) {
+      if (!this.noBlur) {
+        this.notes[index].title = this.initialTitle
+        this.activeIndex = null
+        this.hiddenInput = false
+      }
     },
   },
 }
@@ -78,6 +120,10 @@ export default {
   }
   p {
     color: mediumblue;
+  }
+  input {
+    margin-bottom: 0px;
+    padding: 0px;
   }
   svg {
     margin-right: 12px;
